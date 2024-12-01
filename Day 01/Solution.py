7eb18b235009a55946eb9a9bdf4747910a14555d
3saster <paulbologea@hotmail.com>
2024-12-01 11:23:32 -0800

    Day 01

---
+++ Day 01/Solution.py
@@ -0,0 +1,15 @@
+from collections import Counter
+
+def Part1(data):
+    data = [[int(n) for n in l.split()] for l in data]
+    first  = sorted( [d[0] for d in data] )
+    second = sorted( [d[1] for d in data] )
+
+    return sum( abs(s-f) for f,s in zip(first,second) )
+
+def Part2(data):
+    data = [[int(n) for n in l.split()] for l in data]
+    first  = [d[0] for d in data]
+    second = Counter([d[1] for d in data])
+    
+    return sum( f*second[f] for f in first )
