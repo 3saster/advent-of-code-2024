0af50f5a67e49807915b4f34b7d522201b986da5
3saster <paulbologea@hotmail.com>
2024-12-02 22:02:01 -0800

    Day 03

---
+++ Day 03/Solution.py
@@ -0,0 +1,12 @@
+import re
+
+def Part1(data):
+    data = "".join(data)
+    pairs = re.findall("mul\((\d+),(\d+)\)",data)
+    return sum( int(n[0])*int(n[1]) for n in pairs )
+
+def Part2(data):
+    data = "".join(data)
+    data = data.split("do()")
+    relevantData = [d.split("don't()")[0] for d in data]
+    return Part1(relevantData)
